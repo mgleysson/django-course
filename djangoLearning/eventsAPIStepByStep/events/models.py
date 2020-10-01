3d75987ea5a4e370a394c4c06651fdeb2925fdbd
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2020-09-30 23:53:24 -0300

    Adiciona model Event e altera idioma e fuso horário.

--- djangoLearning/eventsAPIStepByStep/events/models.py
+++ djangoLearning/eventsAPIStepByStep/events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+
+    priorities_list = (
+        ('0', 'Sem prioridade'),
+        ('1', 'Normal'),
+        ('2', 'Urgente'),
+        ('3', 'Muito Urgente')
+    )
+
+    event = models.CharField(max_length=80)
+    date = models.DateField()
+    priority = models.CharField(max_length=1, choices=priorities_list)
+
+    def __str__(self):
+        return self.event
