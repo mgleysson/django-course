3691ca0001c77c9afaaae328d7f5fcf1c9531181
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2020-09-30 22:01:05 -0300

    Incrementa exemplos de Python sobre condicionais e loops.

--- pythonLearning/files/conditionals_3.py
+++ pythonLearning/files/conditionals_3.py
@@ -1,6 +1,6 @@
 age = input('Qual a sua idade: ')
 age = int(age)
-print('Sua idade é %d' % (age))
+print('Sua idade é %d' % age)
 
 # 0 <= idade < 18 - Criança
 # 18 <= idade < 60 - Adulto
@@ -12,3 +12,21 @@ elif 18 <= age < 60:
     print('Adulto')
 else:
     print('Idoso')
+
+print('------------------')
+
+weight = input('Qual o seu peso: ')
+weight = float(weight)
+
+height = input('Qual a sua altura: ')
+height = float(height)
+
+imc = weight/(height**2)
+print('Seu imc é %.2f' % imc)
+print('Seu imc é', round(imc, 2))
+
+category = 'Não-Magreza'
+if imc < 18.5:
+    category = 'Magreza'
+
+print('Classificação: %s' % category)
--- pythonLearning/files/loops_5.py
+++ pythonLearning/files/loops_5.py
@@ -18,7 +18,24 @@ while element < 5:
 
 print('-------')
 
-# Outros Comandos
+# Outros Comandos: Break
+'''
+O uso mais comum para break é quando alguma condição externa é acionada exigindo uma saída rápida de um loop.
+A instrução break pode ser usada em loops while e for.
+
+Se você estiver usando loops aninhados, a instrução break interrompe a execução do loop mais interno
+e começa a executar a próxima linha de código após o bloco.
+'''
+
+print('---- Início do Loop')
+for letter in 'python':
+    if letter == 'h':
+        break
+    print('Letra corrente :', letter)
+print('---- Fim do Loop')
+
+print('-------')
+
 element = 1
 while element < 5:
     print(element)
@@ -28,7 +45,15 @@ while element < 5:
 
 print('-------')
 
-element = 1
+# Outros comandos: Continue
+'''
+Ele retorna o controle para o início do loop while. 
+A instrução continue rejeita todas as instruções restantes na iteração atual do loop e move o controle de volta para o topo do loop.
+
+A instrução continue pode ser usada em loops while e for .
+'''
+
+element = 0
 while element < 5:
     element += 1
     if element == 3:
