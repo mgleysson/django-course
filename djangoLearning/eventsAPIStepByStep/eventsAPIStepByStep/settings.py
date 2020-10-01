9914bd212e6ce2f9922101aa20952db74d389d6b
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2020-10-01 00:42:09 -0300

    Adiciona rest_framework ao settings, cria serializador e view iniciais.

--- djangoLearning/eventsAPIStepByStep/eventsAPIStepByStep/settings.py
+++ djangoLearning/eventsAPIStepByStep/eventsAPIStepByStep/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events.apps.EventsConfig'
+    'events.apps.EventsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
