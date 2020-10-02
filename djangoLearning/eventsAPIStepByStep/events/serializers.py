426cd2e04946d077a440288297e73102347d7497
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2020-10-01 23:57:05 -0300

    Juntando eventos e comentários na saída do serializador.

--- djangoLearning/eventsAPIStepByStep/events/serializers.py
+++ djangoLearning/eventsAPIStepByStep/events/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 from .models import Event, Comment
 
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('id', 'event', 'date', 'text_priority')
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'author', 'email', 'text', 'date', 'event')
+
+
+class EventSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = ('id', 'event', 'date', 'text_priority', 'comments')
+
+
+
