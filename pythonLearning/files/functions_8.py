8006bb8a594f82dc1be81c8b9d1e7b8dc085a9e9
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2020-10-03 23:44:07 -0300

    Adicionando mais exemplos na aula de funções em Python.

--- pythonLearning/files/functions_8.py
+++ pythonLearning/files/functions_8.py
@@ -11,8 +11,7 @@ def print_hello():
     print('Hello')
 
 
-ret = print_hello()
-print(ret)
+print_hello()
 
 
 # Funcoes com um argumento
@@ -25,7 +24,7 @@ print_greetings(name='Marcos')
 
 # Funções com n argumentos posicionais
 def print_numbers(n1, n2):
-    print('Num. 1: %f, Num. 2: %f' % (n1, n2))
+    print('Num. 1: %.2f, Num. 2: %.3f' % (n1, n2))
 
 
 print_numbers(n1=5.4, n2=5)
@@ -40,20 +39,77 @@ def print_greetings_2(name, upper_case=False):
 
 print_greetings_2('Marcos', upper_case=True)
 
+# Uso de funcoes para organizar código e evitar duplicações
+
+# Sem funcao
+lista1 = [1, 2, 3]
+sum_list = sum(lista1)
+size = len(lista1)
+avg = sum_list / size
+print('A média dos elementos é: ', avg)
+
+lista2 = [4, 5, 6]
+sum_list = sum(lista2)
+size = len(lista2)
+avg = sum_list / size
+print('A média dos elementos é: ', avg)
+
+
+# Com funcao
+def calculate_avg(list_num):
+    sum_list = sum(list_num)
+    size = len(list_num)
+    avg = sum_list / size
+    print('A média dos elementos é: ', avg)
+
+
+lista1 = [1, 2, 3]
+calculate_avg(lista1)
+
+lista2 = [4, 5, 6]
+calculate_avg(lista2)
+
+print('------------')
+
+
 # Escopo de funções
 
-var_1 = 5
+# 1: Dentro da função, pode acessar variávels locais (analogia do vidro fumê)
+def my_func():
+    # escopo local
+    print('Variável global:', x)
 
 
-def local_scope():
-    var_2 = 6
-    global var_1
-    print('Var 2 Local', var_2)
-    print('Var 1 Global', var_1)
-    var_1 = 50
-    # print('Var 1 Local', var_1)
+# escopo global
+x = 20
+my_func()
+print('Valor fora da função:', x)
+
+print('-----------')
+
+# 2: Embora com o mesmo nome, váriaveis declaradas dentro de funções (locais) são diferentes das declaradas fora (globais).
+def my_func():
+    # 2: escopo local
+    x = 10
+    print('Valor dentro da função:', x)
 
 
-print('Before Function Call - Var 1 Global', var_1)
-local_scope()
-print('After Function Call - Var 1 Global', var_1)
+# 1: escopo global
+x = 20
+my_func()
+print('Valor fora da função:', x)
+
+print('-----------')
+
+# 3: Pode se referir a uma variável global e alterá-la diretamente com  a palavra-chave 'global'
+def my_func():
+    # 2: escopo local
+    global x
+    x = 10
+    print('Valor dentro da função:', x)
+
+
+# 1: escopo global
+x = 20
+my_func()
+print('Valor fora da função:', x)
